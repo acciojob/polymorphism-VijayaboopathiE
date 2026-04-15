8a0c735393bb4387a68a77551adbf384b7c2f245
Vijayaboopathi Easwaramoorthi <142922585+VijayaboopathiE@users.noreply.github.com>
2026-04-15 20:52:31 +0530

    Initial commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,14 +3,13 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Product p = new Product();
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter int x,y,z");
-        int x=sc.nextInt();
-        int y=sc.nextInt();
-        int z=sc.nextInt();
-        System.out.println("Enter double a,b");
-        double a=sc.nextDouble();
-        double b=sc.nextDouble();
+
+        int x=10;
+        int y=20;
+        int z=30;
+
+        double a=17.5;
+        double b=23.5;
         int w=p.product(x,y);
         System.out.println(w);
         System.out.println(p.product(x,y,z));
